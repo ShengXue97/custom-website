1878c183ad925f3f5a77ab35ba40a4eca15a20d0
ShengXue97 <simshengxue1997@gmail.com>
2020-06-02 12:00:11 +0800

    Updates

---
+++ precache-manifest.da38cd9a572114e8778dc1544c0ee5b0.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52984f627171bd174b29fc69e2328d22",
+    "url": "/youzu-react/index.html"
+  },
+  {
+    "revision": "8a155144e8ee2aaa7544",
+    "url": "/youzu-react/static/css/7.fa185f22.chunk.css"
+  },
+  {
+    "revision": "a49690ce5a589a2d9318",
+    "url": "/youzu-react/static/css/main.9634d41d.chunk.css"
+  },
+  {
+    "revision": "1ad5cbe5d496970c66e0",
+    "url": "/youzu-react/static/js/0.a4c55d78.chunk.js"
+  },
+  {
+    "revision": "0b2b3a883e3f313f5630",
+    "url": "/youzu-react/static/js/1.125c168c.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/youzu-react/static/js/1.125c168c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60578cfaf8610436cfc6",
+    "url": "/youzu-react/static/js/10.fdabc7d4.chunk.js"
+  },
+  {
+    "revision": "f254f8d88cc56bb457e2",
+    "url": "/youzu-react/static/js/11.b1ec8e55.chunk.js"
+  },
+  {
+    "revision": "d55d1544b364554dc511",
+    "url": "/youzu-react/static/js/12.1c89facd.chunk.js"
+  },
+  {
+    "revision": "4ae51002318b53906b92",
+    "url": "/youzu-react/static/js/13.e7e80905.chunk.js"
+  },
+  {
+    "revision": "1abc927a21d3637cc6cc",
+    "url": "/youzu-react/static/js/14.72230133.chunk.js"
+  },
+  {
+    "revision": "dc814a5149107d25e2f2",
+    "url": "/youzu-react/static/js/15.9b6dd90f.chunk.js"
+  },
+  {
+    "revision": "080e21f7726d055824a3",
+    "url": "/youzu-react/static/js/16.1864216f.chunk.js"
+  },
+  {
+    "revision": "a93d90d2132c1610a27a",
+    "url": "/youzu-react/static/js/17.922c04dc.chunk.js"
+  },
+  {
+    "revision": "c5586cdd1302840ef5d2",
+    "url": "/youzu-react/static/js/18.ba4174f6.chunk.js"
+  },
+  {
+    "revision": "6dc0a3852b063266a2be",
+    "url": "/youzu-react/static/js/19.a5c9443e.chunk.js"
+  },
+  {
+    "revision": "e9e4931d56340d4a2dfe",
+    "url": "/youzu-react/static/js/2.31c86ca8.chunk.js"
+  },
+  {
+    "revision": "a5ad5727bdb7a1e5ec01",
+    "url": "/youzu-react/static/js/20.61ffa88c.chunk.js"
+  },
+  {
+    "revision": "eeb1294845c7a3c25174",
+    "url": "/youzu-react/static/js/21.76520e3f.chunk.js"
+  },
+  {
+    "revision": "7e01665b22bf98b94506",
+    "url": "/youzu-react/static/js/22.d08a5aaf.chunk.js"
+  },
+  {
+    "revision": "48763b73d8c195527ebddd5857148be0",
+    "url": "/youzu-react/static/js/22.d08a5aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618652ac502393576610",
+    "url": "/youzu-react/static/js/23.68bcc0f4.chunk.js"
+  },
+  {
+    "revision": "a2076536d6bba4983586",
+    "url": "/youzu-react/static/js/24.b2df59ac.chunk.js"
+  },
+  {
+    "revision": "c491749edc4dbeb00ca7",
+    "url": "/youzu-react/static/js/25.1fe4b825.chunk.js"
+  },
+  {
+    "revision": "96db73db8509c62d7540",
+    "url": "/youzu-react/static/js/26.66f7dcae.chunk.js"
+  },
+  {
+    "revision": "a98abc8cb656d31a1be1",
+    "url": "/youzu-react/static/js/27.640115d0.chunk.js"
+  },
+  {
+    "revision": "9749d025d6b9e923ee72",
+    "url": "/youzu-react/static/js/28.e89ede5a.chunk.js"
+  },
+  {
+    "revision": "c54e4faf8456984e3746",
+    "url": "/youzu-react/static/js/29.e061ec07.chunk.js"
+  },
+  {
+    "revision": "8a4a6b71dc325430fc67",
+    "url": "/youzu-react/static/js/3.dd517d42.chunk.js"
+  },
+  {
+    "revision": "4d7991decb6c43dc080b",
+    "url": "/youzu-react/static/js/30.19ec444b.chunk.js"
+  },
+  {
+    "revision": "b4e2ca4fcf5e6977a833",
+    "url": "/youzu-react/static/js/31.0fbcdb4e.chunk.js"
+  },
+  {
+    "revision": "38c4cb6908ffb416e35e",
+    "url": "/youzu-react/static/js/32.7e20392c.chunk.js"
+  },
+  {
+    "revision": "47e2cbaa2b769ec93a57",
+    "url": "/youzu-react/static/js/33.24c05883.chunk.js"
+  },
+  {
+    "revision": "2f39cad0e72bed47e217",
+    "url": "/youzu-react/static/js/34.88ef9b30.chunk.js"
+  },
+  {
+    "revision": "81bf7427116b7fb90ce7",
+    "url": "/youzu-react/static/js/35.24e79567.chunk.js"
+  },
+  {
+    "revision": "39b540b5dab920045b3c",
+    "url": "/youzu-react/static/js/36.563c605e.chunk.js"
+  },
+  {
+    "revision": "215d84134bcce68607b6",
+    "url": "/youzu-react/static/js/37.1510ab09.chunk.js"
+  },
+  {
+    "revision": "91358375aeae6ab932f5",
+    "url": "/youzu-react/static/js/38.bce12d8b.chunk.js"
+  },
+  {
+    "revision": "7dbb9efd41ebc5928c5a",
+    "url": "/youzu-react/static/js/39.1b9245e4.chunk.js"
+  },
+  {
+    "revision": "e19434997304f7d1ad05",
+    "url": "/youzu-react/static/js/4.a689c606.chunk.js"
+  },
+  {
+    "revision": "4537d15749eeb9e684e1",
+    "url": "/youzu-react/static/js/40.bba7c8d7.chunk.js"
+  },
+  {
+    "revision": "47c778f9e1e8c74dc586",
+    "url": "/youzu-react/static/js/41.400dc1c8.chunk.js"
+  },
+  {
+    "revision": "23bedb4f9179665e1605",
+    "url": "/youzu-react/static/js/42.4300f61d.chunk.js"
+  },
+  {
+    "revision": "14cd30331061dae48df4",
+    "url": "/youzu-react/static/js/43.f1308098.chunk.js"
+  },
+  {
+    "revision": "a82848d2619368e85a6c",
+    "url": "/youzu-react/static/js/44.a11c13c6.chunk.js"
+  },
+  {
+    "revision": "5e91af242d6fc18e9017",
+    "url": "/youzu-react/static/js/45.84816d9b.chunk.js"
+  },
+  {
+    "revision": "8a155144e8ee2aaa7544",
+    "url": "/youzu-react/static/js/7.c9b28923.chunk.js"
+  },
+  {
+    "revision": "0243d4adc7326426fb27ee1b330bf608",
+    "url": "/youzu-react/static/js/7.c9b28923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72731988e3ddeb2b51e0",
+    "url": "/youzu-react/static/js/8.a55c9859.chunk.js"
+  },
+  {
+    "revision": "5c9b10f97fb4ef33b40be2abacc4f956",
+    "url": "/youzu-react/static/js/8.a55c9859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2a1d194c0810bf14f4",
+    "url": "/youzu-react/static/js/9.1fc379bc.chunk.js"
+  },
+  {
+    "revision": "a49690ce5a589a2d9318",
+    "url": "/youzu-react/static/js/main.bcc16238.chunk.js"
+  },
+  {
+    "revision": "7b4f2e3246654964c083",
+    "url": "/youzu-react/static/js/runtime-main.1ef51a67.js"
+  }
+]);
